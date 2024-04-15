b40301f6086f4af197962a78ea7556e4eb65f2c6
Khutso Kobela <47497650+Takobz@users.noreply.github.com>
2024-04-15 15:38:43 +0000

    feat: add ew text feature

--- prod-workflow-project/frontend/src/App.js
+++ prod-workflow-project/frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Me!
+          A New Feature Is In The App.
         </p>
         <a
           className="App-link"
